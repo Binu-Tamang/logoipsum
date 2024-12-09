51530ca29cc15be5cabe282e4cec2aa53ba30e8c
Binu_Tamang <bunutmg323@gmail.com>
2024-12-10 01:43:40 +0545

    Initial commit on Sign-Up branch

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import Login from './pages/Login';
+
+const App = () => {
+    return (
+        <div>
+            <Login />
+        </div>
+    );
+};
+
+export default App;
